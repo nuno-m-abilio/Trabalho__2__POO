664a714008774dfb9175725fcc720ef68791a97c
Eduardo Minholi <eduarddo.minholi@gmail.com>
2024-09-11 04:27:04 -0300

    Update Tela5SecCadastroConsulta.java

--- Tela POO/Tela5SecCadastroConsulta.java
+++ Tela POO/Tela5SecCadastroConsulta.java
@@ -25,31 +25,31 @@ public class Tela5SecCadastroConsulta extends JFrame{
         add(inicio);
         
         JLabel texto1 = new javax.swing.JLabel();
-        texto1.setText("CPF: ");
+        texto1.setText("Data: ");
         texto1.setBounds(50, 50, 100, 50);
         add(texto1);
         
-        JTextField cpf = new javax.swing.JTextField("Ex: 123.456.789.10");
-        cpf.setBounds(120, 65, 180, 20);
-        add(cpf);
+        JTextField data = new javax.swing.JTextField("Ex: 02/08/2024");
+        data.setBounds(120, 65, 180, 20);
+        add(data);
         
         JLabel texto2 = new javax.swing.JLabel();
-        texto2.setText("Data: ");
+        texto2.setText("Horario: ");
         texto2.setBounds(50, 80, 100, 50);
         add(texto2);
         
-        JTextField data = new javax.swing.JTextField("Ex: 02/08/2024");
-        data.setBounds(120, 95, 180, 20);
-        add(data);
+        JTextField horario = new javax.swing.JTextField("Ex: 17:40");
+        horario.setBounds(120, 95, 180, 20);
+        add(horario);
         
         JLabel texto3 = new javax.swing.JLabel();
-        texto3.setText("Horario: ");
+        texto3.setText("Médico: ");
         texto3.setBounds(50, 110, 100, 50);
         add(texto3);
         
-        JTextField horario = new javax.swing.JTextField("Ex: 17:40");
-        horario.setBounds(120, 125, 180, 20);
-        add(horario);
+        JTextField cpf = new javax.swing.JTextField("Ex: Veronica");
+        cpf.setBounds(120, 125, 180, 20);
+        add(cpf);
         
         JLabel texto4 = new javax.swing.JLabel();
         texto4.setText("Tipo da Consulta:");
@@ -59,13 +59,13 @@ public class Tela5SecCadastroConsulta extends JFrame{
         ButtonGroup grupo1 = new ButtonGroup();
         
         JRadioButton particular = new javax.swing.JRadioButton();
-        particular.setText("Particular");
+        particular.setText("Normal");
         particular.setBounds(50, 180, 100, 20);
         grupo1.add(particular);
         add(particular);
         
         JRadioButton plano = new javax.swing.JRadioButton();
-        plano.setText("Plano");
+        plano.setText("Retorno");
         plano.setBounds(150, 180, 120, 20);
         grupo1.add(plano);
         add(plano);
